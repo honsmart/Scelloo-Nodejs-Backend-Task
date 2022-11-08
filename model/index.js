68e8e412f31d52010e440d57839def92e0f4558a
honsmart <adegboyeopeyemi580@gmail.com>
2022-11-08 02:22:51 +0100

    Fixed issues

--- model/index.js
+++ model/index.js
@@ -8,28 +8,12 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-const url = 'postgres://lyxrqhhnusjets:ba83dd9b9dc43ecd5a08b5c7bf014d649f98dfb720ec6805dd6b78827d0a8771@ec2-54-165-90-230.compute-1.amazonaws.com:5432/d3t8fc3k83a2g5'
 
-if (env === 'production') {
-  // Break apart the Heroku database url and rebuild the configs we need
-  const { DATABASE_URL } = process.env;
-  const dbUrl = url.parse(DATABASE_URL);
-  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-  const password = dbUrl.auth.substr(
-    dbUrl.auth.indexOf(':') + 1,
-    dbUrl.auth.length
-  );
-  const dbName = dbUrl.path.slice(1);
-  const host = dbUrl.hostname;
-  const { port } = dbUrl;
-  config.host = host;
-  config.port = port;
-  sequelize = new Sequelize(dbName, username, password, config);
-}
+
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(dbName, username, password, config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
