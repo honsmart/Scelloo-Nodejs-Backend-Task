35c2a99e3f3ae5e80d97f75ffdb0cd9f3c5cb88d
honsmart <adegboyeopeyemi580@gmail.com>
2022-11-08 02:07:42 +0100

    Fixed issues

--- model/index.js
+++ model/index.js
@@ -8,6 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const url = 'postgres://lyxrqhhnusjets:ba83dd9b9dc43ecd5a08b5c7bf014d649f98dfb720ec6805dd6b78827d0a8771@ec2-54-165-90-230.compute-1.amazonaws.com:5432/d3t8fc3k83a2g5'
 
 if (env === 'production') {
   // Break apart the Heroku database url and rebuild the configs we need
